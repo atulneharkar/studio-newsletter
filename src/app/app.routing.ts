cc7944f2a8ee7118ddbc03ab9cf9a7de399b3697
Nishant Samant <nisamant@deloitte.com>
2018-04-04 12:20:24 +0530

    Added initial dashboard markup and style

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,97 +1,103 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule } from "@angular/router";
 
-import { HomeComponent } from './content/home';
-import { LoginComponent } from './login';
-import { ForgotPasswordComponent, ResetPasswordComponent } from './forgot-password';
-import { UserComponent, UserListComponent } from './content/user';
-import { EventComponent, EventListComponent } from './content/event';
-import { MeetingComponent, MeetingListComponent } from './content/meeting';
-import { InternalProjectComponent, InternalProjectListComponent } from './content/internal-project';
+import { HomeComponent } from "./content/home";
+import { LoginComponent } from "./login";
+import {
+  ForgotPasswordComponent,
+  ResetPasswordComponent
+} from "./forgot-password";
+import { UserComponent, UserListComponent } from "./content/user";
+import { EventComponent, EventListComponent } from "./content/event";
+import { MeetingComponent, MeetingListComponent } from "./content/meeting";
+import {
+  InternalProjectComponent,
+  InternalProjectListComponent
+} from "./content/internal-project";
 
-import { AuthGuard } from './_guards/index';
+import { AuthGuard } from "./_guards/index";
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
-    canActivate: [AuthGuard] 
+    path: "",
+    component: HomeComponent
+    //canActivate: [AuthGuard]
   },
   {
-    path: 'login',
-    component: LoginComponent,
+    path: "login",
+    component: LoginComponent
   },
   {
-    path: 'register',
+    path: "register",
     component: UserComponent
   },
   {
-    path: 'forgot-password',
+    path: "forgot-password",
     component: ForgotPasswordComponent
   },
   {
-    path: 'reset-password/:otp/:userId',
+    path: "reset-password/:otp/:userId",
     component: ResetPasswordComponent
   },
   {
-    path: 'users/edit/:id',
+    path: "users/edit/:id",
     component: UserComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'users',
+    path: "users",
     component: UserListComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'events',
+    path: "events",
     component: EventListComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'events/create',
+    path: "events/create",
     component: EventComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'events/edit/:id',
+    path: "events/edit/:id",
     component: EventComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'meeting',
+    path: "meeting",
     component: MeetingListComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'meeting/create',
+    path: "meeting/create",
     component: MeetingComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'meeting/edit/:id',
+    path: "meeting/edit/:id",
     component: MeetingComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'internal-project',
+    path: "internal-project",
     component: InternalProjectListComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'internal-project/create',
+    path: "internal-project/create",
     component: InternalProjectComponent,
     canActivate: [AuthGuard]
   },
   {
-    path: 'internal-project/edit/:id',
+    path: "internal-project/edit/:id",
     component: InternalProjectComponent,
     canActivate: [AuthGuard]
   },
 
   // otherwise redirect to content
   {
-    path: '**',
-    redirectTo: ''
+    path: "**",
+    redirectTo: ""
   }
 ];
 
