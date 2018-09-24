2904b06a73e5e81a8abc9264a6713db101a4896d
US\hchawla <hchawla@deloitte.com>
2018-09-24 15:23:10 +0530

    events loading on home page

--- src/app/content/home/home.component.ts
+++ src/app/content/home/home.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService, CommonService } from '../../_services';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  public events: Event[] = [];
 
-  constructor() { }
+  constructor(private eventService: EventService,
+      private commonService: CommonService) { }
 
   ngOnInit() {
+  	this.getAllEvents();
+  }
+
+  //method to get event list
+  private getAllEvents() {
+    console.log("getting all events on home page");
+    this.eventService.getAll()
+      .subscribe(
+        events => { 
+          this.events = events;
+          this.events = this.events.reverse();
+          console.log("this.events",this.events);
+        });
   }
 
 }
